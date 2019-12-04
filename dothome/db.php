39d30e8ebb557347efcab6b386d00d80da326dc5
hyemi <devham76@gmail.com>
2019-12-04 16:33:24 +0900

    use php in local

--- dothome/db.php
+++ dothome/db.php
@@ -4,11 +4,18 @@ header('Content-Type: text/html; charset=UTF-8');
 
 $db_host = "localhost"; 
 
-$db_user = "devham76"; 
+$db_user = "root"; 
 
-$db_passwd = "hyemi2668*";
+$db_passwd = "hyemi2668";
 
-$db_name = "devham76"; 
+$db_name = "dio"; 
+
+
+$conn = mysqli_connect($db_host, $db_user, $db_passwd);
+$r = mysqli_select_db($conn, $db_name);
+//$result = mysqli_query($conn, "SELECT * FROM topic");
+
+echo "result : " . $r. "\n";
 
 $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
 
@@ -16,7 +23,7 @@ $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
 
 if (mysqli_connect_errno($conn)) {
 
-echo "데이터베이스 연결 실패: " . mysqli_connect_error();
+echo "\n데이터베이스 연결 실패: " . mysqli_connect_error();
 
 } else {
 
